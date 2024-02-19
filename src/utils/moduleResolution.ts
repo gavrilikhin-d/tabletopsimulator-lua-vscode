f7feb7e8f51c9622f7e898705b01d30bfca6c946
Rolando Romero <contact@rolandostar.com>
2024-02-19 14:14:35 -0600

    JumpToError

--- src/utils/moduleResolution.ts
+++ src/utils/moduleResolution.ts
@@ -9,7 +9,7 @@ export const luaPatterns = ['?.lua', '?.ttslua']
 export const getIncludePaths = (): string[] => [
   // Global Include folder has higher priority
   docsFolder,
-  // TODO: Config Paths...,
+  // Config Paths...,
   // Workspace Paths, starting by current workdir
   getWorkDir().fsPath,
   ...(
